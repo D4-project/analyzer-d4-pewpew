7f14d5ce317c405eb82e40554573d135e86221dd
Jean-Louis Huynen <huynenjl@gmail.com>
2020-09-30 15:34:32 +0200

    add: [client-build] adds the build to ease deployment

---
+++ build/precache-manifest.fb93f6b06e2a2da5dae1eda3fb96d6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88fb7df37632b194cfeb227b7ae19b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea1ea83d2eab2c6d302",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25c4838720dd99816a00",
+    "url": "/static/js/2.96fc5e4c.chunk.js"
+  },
+  {
+    "revision": "17f1f991b6c7577feba4c938911e59e7",
+    "url": "/static/js/2.96fc5e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea1ea83d2eab2c6d302",
+    "url": "/static/js/main.2ee55eac.chunk.js"
+  },
+  {
+    "revision": "72c1cfac5bf4dd903e98",
+    "url": "/static/js/runtime-main.c1478ae2.js"
+  }
+]);
