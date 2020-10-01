c1df971b8b6c30edfc32944c9168db9d6f9e7ab4
Jean-Louis Huynen <huynenjl@gmail.com>
2020-10-01 11:36:42 +0200

    chg: [client] consume daily events

--- pewpew/src/App.js
+++ pewpew/src/App.js
@@ -1,8 +1,10 @@
 /* global window */
 import React, { Component } from 'react';
-import DeckGL, {GeoJsonLayer, ArcLayer} from 'deck.gl';
+import DeckGL, { GeoJsonLayer, ArcLayer, TRANSITION_EVENTS } from 'deck.gl';
+import ReactMapGL, { FlyToInterpolator } from 'react-map-gl';
 
-const COUNTRIES = window.location + 'map/ne_50m_admin_0_scale_rank.geojson' ;
+const COUNTRIES = window.location + 'map/ne_50m_admin_0_scale_rank.geojson';
+const DAILY = window.location + "daily.json";
 const D4 = [];
 
 export default class App extends Component {
@@ -21,6 +23,10 @@ export default class App extends Component {
     };
     this._getData = this._getData.bind(this);
     this._processData = this._processData.bind(this);
+    this._resize = this._resize.bind(this);
+
+    this._interruptionStyle = TRANSITION_EVENTS.BREAK;
+    this._onViewStateChange = this._onViewStateChange.bind(this);
   }
 
   _getData() {
@@ -37,11 +43,11 @@ export default class App extends Component {
 
     var ws = new WebSocket(uri)
 
-    ws.onopen = function() {
+    ws.onopen = function () {
       console.log('Connected')
     }
 
-    ws.onmessage = function(ev){
+    ws.onmessage = function (ev) {
       var json = JSON.parse(ev.data);
       D4.push(json[0]);
       up._processData();
@@ -49,25 +55,46 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+    // js binding 
+    var up = this;
+    fetch(DAILY)
+      .then(response => response.text())
+      .then((data) => {
+        if (data) {
+          var events = data.split(/\r\n|\r|\n/g);
+          events.forEach(function (event) {
+            if (event) {
+              var json = JSON.parse(event);
+              D4.push(json[0]);
+            }
+          });
+          up._processData();
+        }
+      }, (err) => {
+        console.log('Could not fetch ' + DAILY + ' ' + err); // Erreur !
+      });
+
     this._getData();
-    this._processData();
     window.addEventListener('resize', this._resize);
     this._resize();
   }
 
   _processData() {
     if (D4) {
-      console.log(D4);
       const points = D4.reduce((accu, curr) => {
-        console.log(curr)
         accu.push({
           position: [Number(curr.geoip_lon), Number(curr.geoip_lat)],
         });
         return accu;
       }, []);
+      // Add the point to the ARCS
       this.setState({
         points,
       });
+      // Move the viewport to the last point
+      var last = points[points.length - 1];
+      console.log(last)
+      this._easeTo(last.position[0], last.position[1])
     }
   }
 
@@ -75,10 +102,30 @@ export default class App extends Component {
     window.removeEventListener('resize', this._resize);
   }
 
+  _easeTo({ longitude, latitude }) {
+    this.setState({
+      viewState: {
+        ...this.state.viewState,
+        longitude,
+        latitude,
+        zoom: 11,
+        pitch: 0,
+        bearing: 0,
+        transitionDuration: 'auto',
+        transitionInterpolator: new FlyToInterpolator({ speed: 2 }),
+        transitionInterruption: this._interruptionStyle
+      }
+    });
+  }
+
   _onViewportChange = (viewport) => {
     this.setState({
       viewport: { ...this.state.viewport, ...viewport }
     });
+  }
+
+  _onViewStateChange({ viewState }) {
+    this.setState({ viewState });
   }
 
   _resize = () => {
@@ -89,28 +136,29 @@ export default class App extends Component {
   }
 
   render() {
+    const { viewState } = this.state;
     return (
-    <DeckGL controller={true} initialViewState={this.state.viewport}>
-      <GeoJsonLayer
-        id="base-map"
-        data={COUNTRIES}
-        stroked={true}
-        filled={true}
-        lineWidthMinPixels={2}
-        opacity={0.4}
-        getLineColor={[60, 60, 60]}
-        getFillColor={[200, 200, 200]}
-      />
-      <ArcLayer
-        id="arcs"
-        data={this.state.points}
-        getSourcePosition={f => [6.1319346, 49.611621]}
-        getTargetPosition={f => f.position} 
-        getSourceColor={[0, 128, 200]}
-        getTargetColor={[200, 0, 80]}
-        getWidth={1}
-      />
-    </DeckGL>
+      <DeckGL controller={true} initialViewState={this.state.viewport}>
+        <GeoJsonLayer
+          id="base-map"
+          data={COUNTRIES}
+          stroked={true}
+          filled={true}
+          lineWidthMinPixels={2}
+          opacity={0.4}
+          getLineColor={[60, 60, 60]}
+          getFillColor={[200, 200, 200]}
+        />
+        <ArcLayer
+          id="arcs"
+          data={this.state.points}
+          getSourcePosition={f => [6.1319346, 49.611621]}
+          getTargetPosition={f => f.position}
+          getSourceColor={[0, 128, 200]}
+          getTargetColor={[200, 0, 80]}
+          getWidth={1}
+        />
+      </DeckGL>
     );
   }
 }
